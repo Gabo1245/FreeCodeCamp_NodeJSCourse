d3eef5d2616c9d90a52d40a68bf44b4001f30c55
Gabo1245 <ponce.pgabriel@gmail.com>
2022-02-27 12:55:23 -0600

    Course up to 5:43:10

--- express/app.js
+++ express/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const {readFileSync} = require('fs')
 
 const app = express()
 
@@ -11,7 +12,7 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
     
     res.set('content-type', 'text/html')
-    res.send('<title> About Page </title> <h1> About this tutorial </h1> <h2> Lorem ipsum ... </h2>')
+    res.status(200).send('<title> About Page </title> <h1> About this tutorial: </h1> <h2> Lorem ipsum ... </h2>')
 
 })
 
@@ -28,6 +29,12 @@ app.get('/api/v1', (req, res) => {
 
     res.status(200).send(def_object)
 })
+
+app.get('*', (req, res) => {
+    res.set('content-type', 'text/html')
+    res.status(404).send(readFileSync('./error404.html'))
+})
+
 
 app.listen(5000, () => {
     console.log('Server is listening on port 5000')
